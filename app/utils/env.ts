b49c71c47a19b601fa26428af9193573c1901ac3
Seth Stahl <sethstahly02@gmail.com>
2025-06-08 12:35:34 +1000

    fix(structure): clean root and set vercel root to submodule

--- app/utils/env.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-import path from 'path';
-
-export function loadEnvVars() {
-  if (process.env.NODE_ENV === 'development') {
-    dotenv.config({ path: path.resolve(process.cwd(), '.env.local') });
-  }
-  dotenv.config({ path: path.resolve(process.cwd(), '.env') });
-} 
